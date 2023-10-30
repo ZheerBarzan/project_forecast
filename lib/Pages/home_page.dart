7a5128a9775dcdfaf0165205dea8063a20a7fcff
ZheerBarzan <zheerbarzan85@outlook.com>
2023-10-30 14:19:02 +0300

    made an initial state to fetch the weather

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project_forecast/Model/weather_model.dart';
+import 'package:project_forecast/Service/weather_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +10,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//api key
+  final _weatherService =
+      WeatherService(apiKey: "d34313ac99bf19dfb883cd67e588c43b");
+  Weather? _weather;
+
+// fetch the weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentLocation();
+
+    // get the weather for the City
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+//weather animations
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
