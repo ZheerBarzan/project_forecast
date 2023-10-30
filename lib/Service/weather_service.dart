a749b20085bd3389811b0159ec9b712f3a98e234
ZheerBarzan <zheerbarzan85@outlook.com>
2023-10-30 12:57:03 +0300

    making the weather model and implementing the API

---
+++ lib/Service/weather_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:project_forecast/Model/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const BASE_URL = "https://api.openweathermap.org/data/3.0/weather";
+  final String apiKey;
+
+  WeatherService({required this.apiKey});
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.formJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to Load');
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_forecast/home_page.dart';
+import 'package:project_forecast/Pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
