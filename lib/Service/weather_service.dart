3570c2cedca8ed540927d24be32ed11fe9030e92
ZheerBarzan <zheerbarzan85@outlook.com>
2023-10-30 14:08:51 +0300

    getting location permission from the user

--- lib/Service/weather_service.dart
+++ lib/Service/weather_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:project_forecast/Model/weather_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -18,5 +20,28 @@ class WeatherService {
     } else {
       throw Exception('Failed to Load');
     }
+  }
+
+  Future<String> getCurrentLocation() async {
+    // getting permission from the user
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    // fetch the current location
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // convert the Location into a List of placemark object
+
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    //Extract the city name from the first place mark
+
+    String? city = placemark[0].locality;
+
+    return city ?? "";
   }
 }
