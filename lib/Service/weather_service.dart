7de029fe5464b0c2658af36f37fa6b4bc4916b05
ZheerBarzan <zheerbarzan85@outlook.com>
2023-11-02 17:11:22 +0300

    fixed the issue with the APi now it showes the place clearly

--- lib/Service/weather_service.dart
+++ lib/Service/weather_service.dart
@@ -6,7 +6,7 @@ import 'package:project_forecast/Model/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = "https://api.openweathermap.org/data/3.0/weather";
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
   WeatherService({required this.apiKey});
